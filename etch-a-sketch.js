72d760a503715864e62aa69d0c0bbdcc9635d12b
thesakshamm <sakshamstha5514@gmail.com>
2024-04-20 23:56:06 +0545

    add reset button, start point, end point and color chagner function

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -2,19 +2,37 @@ let boxes = document.querySelectorAll(".box")
 
 
 
+boxes.forEach((box)=>{
+    box.addEventListener("click", () => {colorChanger();box.style.backgroundColor = "rgb(0, 0, 0)"})
+})//starting point for sketch
 
-function colorChanger()
+
+function colorChanger()//changes color of the box on when mouse is hovered
 {
     boxes.forEach((box) => {
         box.addEventListener("mouseover", () => { 
-            if(window.getComputedStyle(box).backgroundColor == "rgb(255, 255, 255)") {
+            if(window.getComputedStyle(box).backgroundColor == "rgb(255, 255, 255)") 
+            {
                 box.style.backgroundColor = "rgb(0, 0, 0)";
-                console.log("clicked");
-            } else {
-                box.style.backgroundColor = "rgb(255, 255, 255)";
-                console.log("clicked");
+                
             }
+
+            
+            
         });
     });
 }
+
+function stopPoint()
+{
+    boxes.forEach((box)=>{
+        box.addEventListener("click", () => {})
+    })
+}
+
+let resetBtn = document.querySelector("#resetBtn")
+
+resetBtn.addEventListener("click", () =>{boxes.forEach((box)=>
+{box.style.backgroundColor = "rgb(255, 255, 255)"})
+})
 
